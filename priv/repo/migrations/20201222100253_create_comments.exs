3ed2e8e1abf23f02ae3ba5d8b326251a14933aea
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-22 12:08:38 +0200

    add comment schema

---
+++ priv/repo/migrations/20201222100253_create_comments.exs
@@ -0,0 +1,16 @@
+defmodule Tex.Repo.Migrations.CreateComments do
+  use Ecto.Migration
+
+  def change do
+    create table(:comments) do
+      add :content, :text
+      add :post_id, references(:posts, on_delete: :delete_all)
+      add :user_id, references(:users, on_delete: :delete_all)
+
+      timestamps()
+    end
+
+    create index(:comments, [:post_id])
+    create index(:comments, [:user_id])
+  end
+end
--- test/tex/articles_test.exs
+++ test/tex/articles_test.exs
@@ -179,4 +179,63 @@ defmodule Tex.ArticlesTest do
       assert %Ecto.Changeset{} = Articles.change_view(view)
     end
   end
+
+  describe "comments" do
+    alias Tex.Articles.Comment
+
+    @valid_attrs %{content: "some content"}
+    @update_attrs %{content: "some updated content"}
+    @invalid_attrs %{content: nil}
+
+    def comment_fixture(attrs \\ %{}) do
+      {:ok, comment} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Articles.create_comment()
+
+      comment
+    end
+
+    test "list_comments/0 returns all comments" do
+      comment = comment_fixture()
+      assert Articles.list_comments() == [comment]
+    end
+
+    test "get_comment!/1 returns the comment with given id" do
+      comment = comment_fixture()
+      assert Articles.get_comment!(comment.id) == comment
+    end
+
+    test "create_comment/1 with valid data creates a comment" do
+      assert {:ok, %Comment{} = comment} = Articles.create_comment(@valid_attrs)
+      assert comment.content == "some content"
+    end
+
+    test "create_comment/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Articles.create_comment(@invalid_attrs)
+    end
+
+    test "update_comment/2 with valid data updates the comment" do
+      comment = comment_fixture()
+      assert {:ok, %Comment{} = comment} = Articles.update_comment(comment, @update_attrs)
+      assert comment.content == "some updated content"
+    end
+
+    test "update_comment/2 with invalid data returns error changeset" do
+      comment = comment_fixture()
+      assert {:error, %Ecto.Changeset{}} = Articles.update_comment(comment, @invalid_attrs)
+      assert comment == Articles.get_comment!(comment.id)
+    end
+
+    test "delete_comment/1 deletes the comment" do
+      comment = comment_fixture()
+      assert {:ok, %Comment{}} = Articles.delete_comment(comment)
+      assert_raise Ecto.NoResultsError, fn -> Articles.get_comment!(comment.id) end
+    end
+
+    test "change_comment/1 returns a comment changeset" do
+      comment = comment_fixture()
+      assert %Ecto.Changeset{} = Articles.change_comment(comment)
+    end
+  end
 end
