6b43ce708ff2432cda593f55911a210a9c51c570
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-23 03:50:55 +0200

    removed unused

--- lib/tex/friendship/invitations_handling.ex
+++ lib/tex/friendship/invitations_handling.ex
@@ -1,10 +1,9 @@
 defmodule Tex.Friendship.InvitationsHandling do
     alias Tex.Friendship.Invitation
-    alias Tex.Articles.Like
     alias Tex.Repo
     import Ecto.Query
     import Plug.Conn
-    
+
     def unconfirmed_invitations(conn, _) do
         query = from(i in Invitation, where: i.status == false, preload: [invitor: :account, invitee: :account])
         unconfirmed_invitations = Repo.all(query)
