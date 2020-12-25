8516e4c46c2d819c4e863d893fd539cb573f0b90
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-25 11:32:07 +0200

    add config for cloudinary

--- config/config.exs
+++ config/config.exs
@@ -27,9 +27,9 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :cloudex,
-    api_key: System.get_env(API_KEY),
-    secret: System.get_env(SECRET),
-    cloud_name: System.get_env(CLOUD_NAME)
+    api_key: System.get_env("CLOUDEX_API_KEY"),
+    secret: System.get_env("CLOUDEX_SECRET"),
+    cloud_name: System.get_env("CLOUDEX_CLOUD_NAME")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
